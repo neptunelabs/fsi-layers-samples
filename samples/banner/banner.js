472491ea90e42df6ee0dc16a8b84ead839d93c01
biernat <biernat@neptunelabs.com>
2022-11-25 16:09:14 +0100

    banner draft

---
+++ samples/banner/banner.js
@@ -0,0 +1,63 @@
+function animateBanner() {
+  new BannerAnimation()
+}
+
+class BannerAnimation {
+
+  constructor() {
+
+    this.thenTime = Date.now()
+
+    this.xSale = -500
+    this.ySale = -100
+    this.dirSale = 6
+    this.speedSale = 15
+    this.textOpacity = 0
+    this.dirOp = 0.2
+    this.text = 0
+    this.speedText = 300
+
+    this.fsiLayersEl = document.getElementById('bannerFSILayer')
+
+    this.moveLayers()
+  }
+
+  moveLayers() {
+
+    this.rafCallID = requestAnimationFrame(() => this.moveLayers())
+
+    let nowTime = Date.now()
+    let elapsedTime = nowTime - this.thenTime
+
+    this.fsiLayersEl.setProperties(['sale'], {
+      bottom: this.xSale,
+      left: this.ySale,
+    })
+
+    this.fsiLayersEl.setProperties(['sale-70'], {
+      opacity: this.textOpacity,
+    })
+
+    this.textOpacity += this.dirOp * (elapsedTime / this.speedText)
+
+    this.xSale += this.dirSale * (elapsedTime / this.speedSale)
+
+    if (this.xSale > -200) {
+      this.speedSale = 0
+      this.fsiLayersEl.setProperties(['sale'], {
+        bottom: -200,
+        left: -100,
+      })
+    }
+    if (this.textOpacity > 1) {
+      this.dirOp = -0.1
+    }
+    if (this.textOpacity < 0.1) {
+      this.dirOp = 0.1
+    }
+
+    this.fsiLayersEl.render()
+
+    this.thenTime = nowTime
+  }
+}
