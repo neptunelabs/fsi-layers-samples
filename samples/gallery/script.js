13b7f2805e75c207ab13a99c6a8b505177fde410
nelabiernat <60930527+nelabiernat@users.noreply.github.com>
2023-12-07 16:54:49 +0100

    converted to png, changed order so that it looks framed and added glass effects

--- samples/gallery/script.js
+++ samples/gallery/script.js
@@ -1,18 +1,18 @@
 imgCounter = 0;
 const imageList = [
-  "images/samples/layers/gallery/art-0.jpg",
-  "images/samples/layers/gallery/art-2.jpg",
-  "images/samples/layers/gallery/art-3.jpg",
-  "images/samples/layers/gallery/art-4.jpg",
-  "images/samples/layers/gallery/art-5.jpg",
-  "images/samples/layers/gallery/art-6.jpg",
-  "images/samples/layers/gallery/art-7.jpg",
-  "images/samples/layers/gallery/art-8.jpg",
-  "images/samples/layers/gallery/art-1.jpg",
-  "images/samples/layers/gallery/art-9.jpg",
-  "images/samples/layers/gallery/art-10.jpg",
-  "images/samples/layers/gallery/art-11.jpg",
-  "images/samples/layers/gallery/art-12.jpg",
+  "images/samples/layers/gallery/art-0.png",
+  "images/samples/layers/gallery/art-2.png",
+  "images/samples/layers/gallery/art-3.png",
+  "images/samples/layers/gallery/art-4.png",
+  "images/samples/layers/gallery/art-5.png",
+  "images/samples/layers/gallery/art-6.png",
+  "images/samples/layers/gallery/art-7.png",
+  "images/samples/layers/gallery/art-8.png",
+  "images/samples/layers/gallery/art-1.png",
+  "images/samples/layers/gallery/art-9.png",
+  "images/samples/layers/gallery/art-10.png",
+  "images/samples/layers/gallery/art-11.png",
+  "images/samples/layers/gallery/art-12.png",
 ]
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -21,14 +21,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const show = () => {
     console.log('show should happen now?')
-    // add art Layers
-    instance.addLayer("container", {"name": 1, "id":"art-1", "src":imageList[0], "left":"23.2%","bottom":"60.5%","width":"22%", "height":"22%", "opactiy" :"0.9"});
-    instance.addLayer("container", {"name": 2, "id":"art-2", "src":imageList[3], "right":"41.4%","bottom":"50.2%","width":"29%", "height":"29%", "opactiy" :"0.9"});
-    instance.addLayer("container", {"name": 3, "id":"art-3", "src":imageList[4 ], "right":"19.5%","bottom":"60.4%","width":"23%", "height":"23%", "opactiy" :"0.9"});
+    //add glass layers
+    instance.addLayer("container", {
+      "name": "glass-left",
+      "src": "images/samples/layers/gallery/glass-top.png",
+      "left":"21.8%","bottom":"59.5%","width":"24%", "height":"24%"
+    });
+    instance.addLayer("container", {
+      "name": "glass-right",
+      "src": "images/samples/layers/gallery/glass-top.png",
+      "right":"18.4%","bottom":"60%","width":"24%", "height":"24%"
+    });
+    // add background Layer in foreground
+    instance.addLayer("container", {
+      "name": "background",
+      "id": "background",
+      "src": "images/samples/layers/gallery/image-wall.png"
+    });
+    instance.addLayer("container", {
+      "name": "glass-center",
+      "src": "images/samples/layers/gallery/glass-top.png",
+      "right":"37.8%","bottom":"47.9%","width":"38%", "height":"38%",
+    });
   }
-
   const instance =  $FSI.createNode("fsi-layers", {
     id: "myLayers",
+    renderer: "png",
     width: '1264px',
     height: '843px',
     skin: 'white',
@@ -48,13 +66,31 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   layersRoot.appendChild(layersContainer);
 
-  const layersBack = $FSI.createNode("fsi-layer", {
-      name: "background",
-      id: "background",
-      src: "images/samples/layers/gallery/inneneinrichtung-mit-fotorahmen.jpg"
+  // add art Layers
+  const layersArt1 = $FSI.createNode("fsi-layer", {
+      name: 1,
+    "src":imageList[0],
+    "left":"21.8%","bottom":"59.5%","width":"24%", "height":"24%", "opactiy" :"0.9"
     }
   );
-  layersContainer.appendChild(layersBack);
+  layersContainer.appendChild(layersArt1);
+
+  const layersArt2 = $FSI.createNode("fsi-layer", {
+      name: 2,
+      "src":imageList[3],
+    "right":"39.7%","bottom":"49.2%","width":"31%", "height":"31%", "opactiy" :"0.9"
+    }
+  );
+  layersContainer.appendChild(layersArt2);
+
+  const layersArt3 = $FSI.createNode("fsi-layer", {
+      name: 3,
+      "src":imageList[4],
+    "right":"18.4%","bottom":"60%","width":"24%", "height":"24%", "opactiy" :"0.9"
+    }
+  );
+  layersContainer.appendChild(layersArt3);
+
 
   // add buttons
   const layersButtonContainer = $FSI.createNode("fsi-layer-group", {
@@ -79,11 +115,11 @@ document.addEventListener('DOMContentLoaded', () => {
   layersButtonContainer.appendChild(layersButton1);
 
   const layersButton2 = $FSI.createNode("fsi-layer", {
-      name: "button2",
+    name: "button2",
     right:"25%",
     bottom:"18%",
-      width:"23%",
-      height:"23%"
+    width:"23%",
+    height:"23%"
     }
   );
   layersButton2.innerHTML = "<button class=\"btnLayers\"  id=\"button-2\" onclick=\"changeArt(2)\">\n" +
